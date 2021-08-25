4dec2d1019bf0bf84417ee8355a44b0141ea8ea7
mangeric <56059523+mangeric@users.noreply.github.com>
2021-08-25 17:50:01 -0300

    Add files via upload

---
+++ Curso de javascript/js/desafio.js
@@ -0,0 +1,44 @@
+
+
+
+var nombre;
+
+const centro_formacion ="eric";
+
+let numero1,numero2,resultado;
+
+let texto1,texto2;
+
+
+/*desafio 1
+
+nombre=prompt("introduzca su nombre");
+
+alert("hola "+nombre +" bienvenido a mi js " +centro_formacion)
+*/
+
+/*desafio 2 
+
+alert("intrduzca un numnero para sumarle 100")
+
+numero1=parseInt(prompt("ingrese su numero"))
+
+numero2=100;
+
+resultado=numero1+numero2;
+
+alert(numero1 + "+" + numero2 +"="+ resultado);
+
+console.log(numero1 + "+" + numero2 +"="+ resultado);
+*/
+
+//desafio 3
+
+alert("introduzca texto 1 y texto 2 para concatenarlos y mostrarlos");
+
+texto1=prompt("introduzca texto 1");
+texto2=prompt("introduzca texto 2");
+alert("Resultado de la concatenacion "+ texto1 + "" + texto2);
+console.log("Resultado de la concatenacion "+ texto1 + "" + texto2);
+
+
